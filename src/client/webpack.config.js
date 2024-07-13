27b7b2dc7b5b68227b8311942f6e4791c5544cfe
Brandon DeCook <350263@wegmans.com>
2024-07-13 09:31:26 -0400

    add folder structure and build configuration

---
+++ src/client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/game.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'client'),
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
